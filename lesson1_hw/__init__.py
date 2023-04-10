04a2a8abd892cdf52991c31383c5d7d41af46ff2
Efremov Evgenii <eefremov@reksoft.ru>
2023-04-10 15:22:23 +0300

    HomeWork_1

---
+++ lesson1_hw/__init__.py
@@ -0,0 +1,4 @@
+# Задача про время:
+hours = int(input())
+minutes = int(input())
+print("Тимофей обычно спит "+ hours * 60 + minutes + "минут")
---
+++ test.py
@@ -0,0 +1,10 @@
+print("Hello, World!")
+x = 5
+print(type(x))
+print(x ** 0.5 ** 4)
+
+a = int(input("Enter a: "))
+if a < x:
+    print(a)
+else:
+    print(x)
